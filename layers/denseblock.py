e6475d3222cd725ddc4a92c871126c637d43a496
Lilith-Sangreal <chibaibuki@outlook.com>
2019-04-08 16:47:30 +0800

    Function: bottleneck denseblock added.

--- layers/denseblock.py
+++ layers/denseblock.py
@@ -19,7 +19,7 @@ class DenseBlock(Layer):
             growth,
             keep,
             use_cudnn_on_gpu=None,
-            data_format=None,
+            bottle_neck=True,
             name='dense_block',
     ):
         super(DenseBlock, self).__init__(prev_layer=prev_layer, name=name)
@@ -37,18 +37,36 @@ class DenseBlock(Layer):
         with tf.variable_scope(name):
             current_layer = prev_layer
             growing_features = in_features
+
             for i in range(block_layers):
-                internal_layer = BatchNormLayer(current_layer,
+                # When bottleneck is unused,
+                # internal in features is always same with growing_features
+                internal_in_features = growing_features
+
+                if bottle_neck is True:
+                    internal_layer = BatchNormLayer(current_layer,
+                                                    is_train=True,
+                                                    act=tf.nn.relu,
+                                                    name=("batchnorm_layer_bn" + str(i)))
+                    internal_layer = Conv2dLayer(internal_layer,
+                                                 shape=(1, 1, internal_in_features, 4 * growth),
+                                                 b_init=None,
+                                                 use_cudnn_on_gpu=use_cudnn_on_gpu,
+                                                 name=("cnn_layer_bn" + str(i)))
+                    internal_in_features = 4 * growth
+
+                internal_layer = BatchNormLayer(internal_layer if bottle_neck else current_layer,
                                                 is_train=True,
                                                 act=tf.nn.relu,
                                                 name=("batchnorm_layer" + str(i)))
                 internal_layer = Conv2dLayer(internal_layer,
-                                             shape=(3, 3, growing_features, growth),
+                                             shape=(3, 3, internal_in_features, growth),
                                              b_init=None,
                                              use_cudnn_on_gpu=use_cudnn_on_gpu,
                                              name=("cnn_layer" + str(i)))
                 internal_layer = DropoutLayer(internal_layer,
                                               keep=keep)
+                
                 current_layer = ConcatLayer([current_layer, internal_layer],
                                             concat_dim=3,
                                             name="concat_layer" + str(i))
