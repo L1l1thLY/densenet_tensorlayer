ff003e115ed795978f56f23dd22c4719d46e2f3e
Lilith-Sangreal <chibaibuki@outlook.com>
2019-04-08 16:23:05 +0800

    Function: compression added.

--- densenet_imagenet.py
+++ densenet_imagenet.py
@@ -7,103 +7,62 @@ tf.logging.set_verbosity(tf.logging.DEBUG)
 tl.logging.set_verbosity(tl.logging.DEBUG)
 
 
-densenet_264_structure = dict(block_1=6, block_2=12, block_3=64, block_4=48, k=24, n_units=1000)
+densenet_264_structure = dict(block1=6, block2=12, block3=64, block4=48,
+                              k=24, n_units=1000)
 
 
-def model_densenet(placeholder_x, placeholder_y_, keep=0.8, structure=densenet_264_structure, use_cudnn_on_gpu=False):
+def model_densenet(placeholder_x, placeholder_y_, keep=0.8, compression=False, structure=densenet_264_structure, use_cudnn_on_gpu=False):
     with tf.variable_scope("densenet_imagenet"):
-        # 224*224
+
         net = tl.layers.InputLayer(placeholder_x, name='input')
         net = tl.layers.Conv2dLayer(net,
-                                    shape=(7, 7, 3, 16),
+                                    shape=(7, 7, 3, 2 * structure['k']),
                                     strides=(1, 2, 2, 1),
                                     b_init=None,
                                     name='conv_in')
-        # 112*112
+
         net = tl.layers.PoolLayer(net,
                                   ksize=(1, 3, 3, 1),
                                   strides=(1, 2, 2, 1),
                                   pool=tf.nn.max_pool,
                                   name='maxpool_in'
                                   )
-        # 56*56
-        net = DenseBlock(net,
-                         block_layers=structure['block_1'],
-                         in_features=16,
-                         growth=structure['k'],
-                         keep=keep,
-                         use_cudnn_on_gpu=use_cudnn_on_gpu,
-                         name='denseblock1'
-                         )
+        features = 2 * structure['k']
 
-        # 56*56
-        features = net.output_features
-        net = tl.layers.BatchNormLayer(net,
-                                       act=tf.nn.relu,
-                                       is_train=True,
-                                       name="batch_norm1"
-                                       )
-        net = tl.layers.Conv2dLayer(net,
-                                    shape=(1, 1, features, features),
-                                    b_init=None,
-                                    use_cudnn_on_gpu=use_cudnn_on_gpu,
-                                    name='conv1')
-        net = tl.layers.PoolLayer(net,
-                                  pool=tf.nn.avg_pool,
-                                  name='pool_layer1')
-        # 28*28
-        net = DenseBlock(net,
-                         block_layers=structure['block_2'],
-                         in_features=features,
-                         growth=structure['k'],
-                         keep=keep,
-                         use_cudnn_on_gpu=use_cudnn_on_gpu,
-                         name='denseblock2'
-                         )
+        for idx in range(3):
+            net = DenseBlock(net,
+                             block_layers=structure['block' + str(idx + 1)],
+                             in_features=features,
+                             growth=structure['k'],
+                             keep=keep,
+                             use_cudnn_on_gpu=use_cudnn_on_gpu,
+                             name='denseblock' + str(idx + 1)
+                             )
+            features = net.output_features
 
-        features = net.output_features
-        net = tl.layers.BatchNormLayer(net,
-                                       act=tf.nn.relu,
-                                       is_train=True,
-                                       name="batch_norm2"
-                                       )
-        net = tl.layers.Conv2dLayer(net,
-                                    shape=(1, 1, features, features),
-                                    b_init=None,
-                                    use_cudnn_on_gpu=use_cudnn_on_gpu,
-                                    name='conv2')
-        net = tl.layers.PoolLayer(net,
-                                  pool=tf.nn.avg_pool,
-                                  name='pool_layer2')
-        # 14*14
+            if compression is True:
+                out_features = features // 2
+            else:
+                out_features = features
+
+            net = tl.layers.BatchNormLayer(net,
+                                           act=tf.nn.relu,
+                                           is_train=True,
+                                           name="batch_norm" + str(idx + 1)
+                                           )
+            net = tl.layers.Conv2dLayer(net,
+                                        shape=(1, 1, features, out_features),
+                                        b_init=None,
+                                        use_cudnn_on_gpu=use_cudnn_on_gpu,
+                                        name='conv' + str(idx + 1))
+            net = tl.layers.PoolLayer(net,
+                                      pool=tf.nn.avg_pool,
+                                      name='pool_layer' + str(idx + 1))
+
+            features = out_features
 
         net = DenseBlock(net,
-                         block_layers=structure['block_3'],
-                         in_features=features,
-                         growth=structure['k'],
-                         keep=keep,
-                         use_cudnn_on_gpu=use_cudnn_on_gpu,
-                         name='denseblock3'
-                         )
-
-        features = net.output_features
-        net = tl.layers.BatchNormLayer(net,
-                                       act=tf.nn.relu,
-                                       is_train=True,
-                                       name="batch_norm3"
-                                       )
-        net = tl.layers.Conv2dLayer(net,
-                                    shape=(1, 1, features, features),
-                                    b_init=None,
-                                    use_cudnn_on_gpu=use_cudnn_on_gpu,
-                                    name='conv3')
-        net = tl.layers.PoolLayer(net,
-                                  pool=tf.nn.avg_pool,
-                                  name='pool_layer3')
-        # 7*7
-
-        net = DenseBlock(net,
-                         block_layers=structure['block_4'],
+                         block_layers=structure['block4'],
                          in_features=features,
                          growth=structure['k'],
                          keep=keep,
@@ -112,7 +71,7 @@ def model_densenet(placeholder_x, placeholder_y_, keep=0.8, structure=densenet_2
                          )
 
         features = net.output_features
-        # 7*7
+
         net = tl.layers.PoolLayer(net,
                                   ksize=(1, 7, 7, 1),
                                   strides=(1, 7, 7, 1),
@@ -124,6 +83,7 @@ def model_densenet(placeholder_x, placeholder_y_, keep=0.8, structure=densenet_2
         net = tl.layers.DenseLayer(net,
                                    n_units=structure['n_units'],
                                    name='output_layer')
+
         y = net.outputs
         y_op = tf.argmax(tf.nn.softmax(y), 1)
 
